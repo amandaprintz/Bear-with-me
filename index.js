c39ff2b271d853efd4988908adaa11ce8a478998
amandaprintz <fannyamandakarlsson@gmail.com>
2021-12-06 15:40:23 +0100

    Cloud img

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 document.querySelector('.box').addEventListener('mousemove', (e) => {
-  //first display top, left values on screen
+  //first display top & left values on screen
 
   console.log(e.clientX, e.clientY);
-  //now add left, top property to the cursor
+  //now add left &  top property to the cursor
   document.querySelector('.cursor').style.left = e.clientX + 'px';
+
   //add top property
   document.querySelector('.cursor').style.top = e.clientY + 'px';
 });
+
+const bears = [
+  { img: './images/drop.gif' },
+  { img: './images/cheers.gif' },
+  { img: './images/bite.gif' },
+  { img: './images/band.gif' },
+  { img: './images/hug.gif' },
+  { img: './images/pat.gif' },
+  { img: './images/petty.gif' },
+  { img: './images/nap.gif' },
+  { img: './images/smooch.gif' },
+  { img: './images/study.gif' },
+  { img: './images/food.gif' },
+];
